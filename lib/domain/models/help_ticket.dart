179cc071376943d562f5b148eb2665886d146383
Makhosandile <56253310+makhosi6@users.noreply.github.com>
2024-02-22 20:00:32 +0200

    Thu Feb 22 20:00:32 SAST 2024 - help object properties

--- lib/domain/models/help_ticket.dart
+++ lib/domain/models/help_ticket.dart
@@ -6,8 +6,9 @@ class HelpTicket {
     this.isResolved,
     this.type,
     this.comment,
+    this.uploads,
     this.preferredCommunicationChannel,
-    this.response,
+    this.responses,
   });
 
   HelpTicket.fromJson(dynamic json) {
@@ -16,12 +17,13 @@ class HelpTicket {
     timestamp = json['timestamp'];
     isResolved = json['isResolved'];
     type = json['type'];
+    uploads = json['uploads'];
     comment = json['comment'] != null ? Comment.fromJson(json['comment']) : null;
     preferredCommunicationChannel = json['preferredCommunicationChannel'];
-    if (json['response'] != null) {
-      response = [];
-      json['response'].forEach((v) {
-        response?.add(Comment.fromJson(v));
+    if (json['responses'] != null) {
+      responses = [];
+      json['responses'].forEach((v) {
+        responses?.add(Comment.fromJson(v));
       });
     }
   }
@@ -31,9 +33,10 @@ class HelpTicket {
   num? timestamp;
   bool? isResolved;
   String? type;
+  List<Upload>? uploads;
   Comment? comment;
   String? preferredCommunicationChannel;
-  List<Comment>? response;
+  List<Comment>? responses;
 
   HelpTicket copyWith({
     String? id,
@@ -41,9 +44,10 @@ class HelpTicket {
     num? timestamp,
     bool? isResolved,
     String? type,
+    List<Upload>? uploads,
     Comment? comment,
     String? preferredCommunicationChannel,
-    List<Comment>? response,
+    List<Comment>? responses,
   }) =>
       HelpTicket(
         id: id ?? this.id,
@@ -51,9 +55,10 @@ class HelpTicket {
         timestamp: timestamp ?? this.timestamp,
         isResolved: isResolved ?? this.isResolved,
         type: type ?? this.type,
+        uploads: uploads ?? this.uploads,
         comment: comment ?? this.comment,
         preferredCommunicationChannel: preferredCommunicationChannel ?? this.preferredCommunicationChannel,
-        response: response ?? this.response,
+        responses: responses ?? this.responses,
       );
 
   Map<String, dynamic> toJson() {
@@ -63,12 +68,13 @@ class HelpTicket {
     map['timestamp'] = timestamp;
     map['isResolved'] = isResolved;
     map['type'] = type;
+    map['uploads'] = uploads;
     if (comment != null) {
       map['comment'] = comment?.toJson();
     }
     map['preferredCommunicationChannel'] = preferredCommunicationChannel;
-    if (response != null) {
-      map['response'] = response?.map((v) => v.toJson()).toList();
+    if (responses != null) {
+      map['responses'] = responses?.map((v) => v.toJson()).toList();
     }
     return map;
   }
@@ -78,26 +84,34 @@ class Comment {
   Comment({
     this.title,
     this.body,
+    this.upload,
   });
 
   Comment.fromJson(dynamic json) {
     title = json['title'];
     body = json['body'];
+    upload = json['upload'];
   }
   String? title;
   String? body;
+  Upload? upload;
   Comment copyWith({
     String? title,
     String? body,
+    Upload? upload,
   }) =>
       Comment(
         title: title ?? this.title,
         body: body ?? this.body,
+        upload: upload ?? this.upload,
       );
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['title'] = title;
     map['body'] = body;
+    map['upload'] = upload;
     return map;
   }
 }
+
+class Upload {}
